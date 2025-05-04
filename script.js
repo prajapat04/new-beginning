cc6fc87b510edd443cd8cbb984008970fc707b55
prajapat04 <124580018+prajapat04@users.noreply.github.com>
2025-05-04 15:55:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ console.log("JavaScript is working!");
     document.getElementById("main-heading").innerText = "You clicked the button!";
   }
   
+  function showName(){
+    const name = document.getElementById("name-input").value;
+    document.getElementById("greeting").innerHTML = `hello ${name}`;
+  }
+
+  window.onload = function() {
+    greetUser();
+
+    const newPara = document.createElement("p");
+    newPara.innerHTML = "This pragraph was added user js";
+    newPara.style.color = "green";
+    document.body.appendChild(newPara);
+  };
+
+  function toggleStyle() {
+    const heading = document.getElementById("toggle-heading");
+    heading.classList.toggle("highlight");
+  }
+
+  function removePra() {
+    const prag = document.getElementById("remove-me");
+    prag.remove();
+  }
