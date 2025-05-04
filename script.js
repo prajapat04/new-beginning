62038d0305f7f84a182f7a7c19211b9176cc31d1
prajapat04 <124580018+prajapat04@users.noreply.github.com>
2025-05-04 15:25:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("JavaScript is working!");
+// function greetUser() {
+//     alert("Welcome to my site!");
+//   }
+//funcation on window load
+//   window.onload = greetUser;
+
+//change hadding in html tag
+  function changeHeading() {
+    document.getElementById("main-heading").innerText = "You clicked the button!";
+  }
+  
